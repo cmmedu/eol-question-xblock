cf296ce095fc95bc888fe69be65ae6940926f2c4
matsalinas <matias.salinas.s@mail.pucv.cl>
2019-08-20 18:47:02 +0000

    Student View

--- eolquestion/eolquestion.py
+++ eolquestion/eolquestion.py
@@ -1,70 +1,81 @@
 """TO-DO: Write a description of what this XBlock is."""
 
 import pkg_resources
+
+from django.template import Context, Template
 from xblock.core import XBlock
-from xblock.fields import Integer, Scope
+from xblock.fields import Integer, String, Scope
 from xblock.fragment import Fragment
+
+# Make '_' a no-op so we can scrape strings
+_ = lambda text: text
 
 
 class EolQuestionXBlock(XBlock):
-    """
-    TO-DO: document what your XBlock does.
-    """
 
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
-
-    # TO-DO: delete count, and define your own fields.
-    count = Integer(
-        default=0, scope=Scope.user_state,
-        help="A simple counter, to show something happening",
+    # TYPE : [Calificada, No Calificada, Control]
+    type = String(
+        display_name = _("Tipo"),
+        help = _("Selecciona el tipo de pregunta"),
+        default = 'Calificada',
+        scope = Scope.settings
     )
+
+    # INDEX
+    index = Integer(
+        display_name = _("Indice"),
+        help = _("Indica el indice de la pregunta"),
+        default = 3,
+        values = { "min" : 1, "step" : 1 },
+        scope = Scope.settings
+    )
+
+    # TEXT
+    text = String(
+        display_name = _("Enunciado"),
+        help = _("Indica el enunciado de la pregunta"),
+        default = 'texto de prueba ',
+        values = { "minlength" : 5 },
+        scope = Scope.settings
+    )
+
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
 
-    # TO-DO: change this view to display your data your own way.
     def student_view(self, context=None):
-        """
-        The primary view of the EolQuestionXBlock, shown to students
-        when viewing courses.
-        """
-        html = self.resource_string("static/html/eolquestion.html")
-        frag = Fragment(html.format(self=self))
+        context_html = self.get_context_student()
+        template = self.render_template('static/html/eolquestion.html', context_html)
+        frag = Fragment(template)
         frag.add_css(self.resource_string("static/css/eolquestion.css"))
         frag.add_javascript(self.resource_string("static/js/src/eolquestion.js"))
         frag.initialize_js('EolQuestionXBlock')
         return frag
 
-    # TO-DO: change this handler to perform your own actions.  You may need more
-    # than one handler, or you may not need any handlers at all.
-    @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
-        """
-        An example handler, which increments the data.
-        """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
+    #TODO: studio_view
 
-        self.count += 1
-        return {"count": self.count}
+    #TODO: studio_submit
 
-    # TO-DO: change this to create the scenarios you'd like to see in the
-    # workbench while developing your XBlock.
+    def get_context_student(self):
+        return {
+            'field_type': self.fields['type'],
+            'field_index': self.fields['index'],
+            'field_text': self.fields['text'],
+            'xblock': self
+        }
+    
+    def render_template(self, template_path, context):
+        template_str = self.resource_string(template_path)
+        template = Template(template_str)
+        return template.render(Context(context))
+
     @staticmethod
     def workbench_scenarios():
         """A canned scenario for display in the workbench."""
         return [
             ("EolQuestionXBlock",
              """<eolquestion/>
-             """),
-            ("Multiple EolQuestionXBlock",
-             """<vertical_demo>
-                <eolquestion/>
-                <eolquestion/>
-                <eolquestion/>
-                </vertical_demo>
              """),
         ]
