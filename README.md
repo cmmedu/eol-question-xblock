b518e3c5d693c9fc3b0747fe05e3ed1af5bc0d3c
matsalinas <matias.salinas.s@mail.pucv.cl>
2019-08-22 20:01:33 +0000

    README

---
+++ README.md
@@ -0,0 +1,5 @@
+# EOL Question XBlock
+XBlock to display "Suma y Sigue" Questions in the [CMM Edu Formación](https://cmmeduformacion.uchile.cl) Open edX LMS. Editable within Open edx Studio.
+
+# Screenshots
+![Screenshot-example](eolquestion/examples/22-08-2019.png)
--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ def package_data(pkg, roots):
 setup(
     name='eolquestion-xblock',
     version='0.1',
-    description='eolquestion XBlock',   # TODO: write a better description.
-    license='UNKNOWN',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
+    description='XBlock con enunciados Suma y Sigue de CMMEduFormacion',
+    license='AGPL v3',         
     packages=[
         'eolquestion',
     ],
