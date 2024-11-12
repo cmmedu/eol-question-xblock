a516a67a350b06ee7b90845d2354aff944b89277
Lozamded <dhiguerafernandez@gmail.com>
2024-11-12 09:14:26 -0300

    chore: Updates latex rendering elements in the statements (#17)
    * Separates render
    * Changes rendered element
    * Bumps version to v1.0.0
    ---------
    
    Co-authored-by: Vicente Daie Pinilla <vdaiep@gmail.com>

--- eolquestion/static/js/src/eolquestion.js
+++ eolquestion/static/js/src/eolquestion.js
@@ -1,20 +1,11 @@
 /* Javascript for EolQuestionXBlock. */
 function EolQuestionXBlock(runtime, element, settings) {
 
-    $(function ($) {
-        /* Here's where you'd do things on page load. */
-        //MathJax.Hub.Queue(["Typeset", MathJax.Hub]); //Reconstruir latex
-
-        var questiontextid = "questionstatement_" + settings.sublocation;
-        renderMathForSpecificElements(questiontextid);
-    });
-
-
     function renderMathForSpecificElements(id) {
         if (typeof MathJax !== "undefined") {
             var $questiontext = $('#' + id);
             if ($questiontext.length) {
-                $questiontext.find('.persistent-btn','modal-content-persistent').each(function (index, questextelem) {
+                $questiontext.find('.text').each(function (index, questextelem) {
                     MathJax.Hub.Queue(["Typeset", MathJax.Hub, questextelem]);
                 });
             }
@@ -22,4 +13,13 @@ function EolQuestionXBlock(runtime, element, settings) {
             console.warn("MathJax no está cargado.");
         }
     }
+
+    $(function ($) {
+        /* Here's where you'd do things on page load. */
+        //MathJax.Hub.Queue(["Typeset", MathJax.Hub]); //Reconstruir latex
+
+        var questiontextid = "questionstatement_" + settings.sublocation;
+        renderMathForSpecificElements(questiontextid);
+    });
 }
+
